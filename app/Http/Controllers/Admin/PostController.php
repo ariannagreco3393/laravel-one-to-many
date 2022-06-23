2ddbffafb68a21e62bffc2de07272a8dde393dee
ariannagreco3393 <a.greco3393@gmail.com>
2022-06-23 18:01:37 +0200

    add exercise

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Post;
+use App\Models\Category;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request; // 👈 Import the Request class
+use Illuminate\Validation\Rule; // 👈 Import the Validation Rule class
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderByDesc('id')->get();
+        //dd($posts);
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        //dd($categories);
+        return view('admin.posts.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\PostRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        //dd($request->all());
+
+        // Validate data
+        $val_data = $request->validated();
+
+        // se l'id esiste tra gli id della tabelal categories
+
+
+        // Gererate the slug
+        $slug = Post::generateSlug($request->title);
+        $val_data['slug'] = $slug;
+
+        //dd($val_data);
+
+        // create the resource
+        Post::create($val_data);
+        // redirect to a get route
+        return redirect()->route('admin.posts.index')->with('message', 'Post Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+
+        $categories = Category::all();
+
+        return view('admin.posts.edit', compact('post', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\PostRequest  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        /* Use the standard Request ☝ */
+        //dd($request->all());
+
+        // validate data
+        //$val_data = $request->validated();
+
+        /* ⚡ Validation unique ⚡*/
+        $val_data = $request->validate([
+            'title' => ['required', Rule::unique('posts')->ignore($post)],
+            'category_id' => 'nullable|exists:categories,id',
+            'cover_image' => 'nullable',
+            'content' => 'nullable'
+        ]);
+
+        //dd($val_data);
+        // Gererate the slug
+        $slug = Post::generateSlug($request->title);
+        //dd($slug);
+        $val_data['slug'] = $slug;
+        // update the resource
+        $post->update($val_data);
+
+        // redirect to get route
+        return redirect()->route('admin.posts.index')->with('message', "$post->title updated successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+
+        $post->delete();
+        return redirect()->route('admin.posts.index')->with('message', "$post->title deleted successfully");
+
+    }
+}
---
+++ resources/js/views/App.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <work-in-progress></work-in-progress>
+
+</template>
+
+
+<script>
+import WorkInProgress from '../components/WorkInProgress';
+export default {
+    name: 'App',
+    components: { WorkInProgress }
+}
+</script>
